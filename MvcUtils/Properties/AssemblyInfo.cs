f03641fcb116ed23c2e4273eacd26613e6b041aa
Chakrit Wichian <chakrit@2nitedesign.com>
2010-07-26 01:27:35 +0700

    Initial solution and project creation.

---
+++ MvcUtils/Properties/AssemblyInfo.cs
@@ -0,0 +1,19 @@
+﻿
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("MvcUtils")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("MvcUtils")]
+[assembly: AssemblyCopyright("Copyright © 2010 2nitedesign! Co., Ltd.")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("79667cb8-6406-44c8-9800-88a9a6fd2347")]
+
+[assembly: AssemblyVersion("0.0.*")]
+[assembly: AssemblyFileVersion("0.0.*")]
