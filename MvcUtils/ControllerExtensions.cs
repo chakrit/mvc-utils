bbb3a6f3426297c404c47f347542f46bc6084284
Chakrit Wichian <chakrit@2nitedesign.com>
2010-09-08 07:54:36 +0700

    Added a strongly-typed RedirectToAction overload for controllers.

---
+++ MvcUtils/ControllerExtensions.cs
@@ -0,0 +1,17 @@
+﻿
+using System;
+using System.Linq.Expressions;
+using System.Web.Mvc;
+
+namespace MvcUtils
+{
+  public static class ControllerExtensions
+  {
+    public static ActionResult RedirectToAction<TController>(this TController controller,
+      Expression<Action<TController>> action)
+      where TController : ControllerBase
+    {
+      return new RedirectToRouteResult(action.ExtractRouteValues());
+    }
+  }
+}
