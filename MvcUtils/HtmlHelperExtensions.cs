bc405fee0436e46233937a53cc9f458f9e909aa5
Chakrit Wichian <chakrit@2nitedesign.com>
2010-09-16 02:19:49 +0700

    Added a BeginForm overload that allows Html attributes

--- MvcUtils/HtmlHelperExtensions.cs
+++ MvcUtils/HtmlHelperExtensions.cs
@@ -8,13 +8,24 @@ namespace MvcUtils
 {
   public static class HtmlHelperExtensions
   {
-    public static MvcForm BeginForm<TController>(this HtmlHelper helper, Expression<Action<TController>> action)
+    public static MvcForm BeginForm<TController>(this HtmlHelper helper,
+      Expression<Action<TController>> action)
       where TController : ControllerBase
     {
       return helper.BeginForm(action.ExtractRouteValues());
     }
 
-    public static MvcHtmlString ActionLink<TController>(this HtmlHelper helper, string linkText, Expression<Action<TController>> action)
+    public static MvcForm BeginForm<TController>(this HtmlHelper helper,
+      Expression<Action<TController>> action, object attributes)
+      where TController : ControllerBase
+    {
+      var values = action.ExtractRouteValues();
+      return helper.BeginForm((string)values["Action"], (string)values["Controller"],
+        values, FormMethod.Post, attributes);
+    }
+
+    public static MvcHtmlString ActionLink<TController>(this HtmlHelper helper,
+      string linkText, Expression<Action<TController>> action)
       where TController : ControllerBase
     {
       var dict = action.ExtractRouteValues();
