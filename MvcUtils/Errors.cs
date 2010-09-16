86a32f2907aac069477504c6348016c9573db28f
Chakrit Wichian <chakrit@2nitedesign.com>
2010-09-17 04:46:18 +0700

    Added an Errors exception helper.

---
+++ MvcUtils/Errors.cs
@@ -0,0 +1,14 @@
+﻿
+using System;
+using System.Web;
+
+namespace MvcUtils
+{
+  public static class Errors
+  {
+    public static Exception NotFound()
+    {
+      return new HttpException(404, "Not Found.");
+    }
+  }
+}
