30906a92e2a6540f0ab7fab191b6388771e9ad5f
Chakrit Wichian <chakrit@2nitedesign.com>
2010-09-29 06:05:29 +0700

    Add UrlHelper extensions

---
+++ MvcUtils/UrlHelperExtensions.cs
@@ -0,0 +1,18 @@
+﻿
+using System;
+using System.Linq.Expressions;
+using System.Web.Mvc;
+
+namespace MvcUtils
+{
+  public static class UrlHelperExtensions
+  {
+    public static string Action<TController>(this UrlHelper helper,
+      Expression<Action<TController>> action)
+      where TController : ControllerBase
+    {
+      var dict = action.ExtractRouteValues();
+      return helper.Action((string)dict["Action"], dict);
+    }
+  }
+}
