fe8b2b8babac370a9ed902e301d7e17045d13d72
Chakrit Wichian <chakrit@2nitedesign.com>
2011-01-27 08:13:15 +0700

    Added an attribute for controller ShowForDisplay and ShowForEdit in model metadata.

---
+++ MvcUtils/ExtendedDataAnnotationsModelMetadataProvider.cs
@@ -0,0 +1,28 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace MvcUtils
+{
+  public class ExtendedDataAnnotationsModelMetadataProvider :
+    DataAnnotationsModelMetadataProvider
+  {
+    protected override ModelMetadata CreateMetadata(
+      IEnumerable<Attribute> attributes, Type containerType,
+      Func<object> modelAccessor, Type modelType,
+      string propertyName)
+    {
+      var metadata = base.CreateMetadata(attributes, containerType,
+        modelAccessor, modelType, propertyName);
+
+      // apply our custom set of attributes
+      var showAttrs = attributes.OfType<ShowAttribute>();
+      foreach (var showAttr in showAttrs)
+        showAttr.Apply(metadata);
+
+      return metadata;
+    }
+  }
+}
---
+++ MvcUtils/ShowAttribute.cs
@@ -0,0 +1,25 @@
+﻿
+using System;
+using System.Web.Mvc;
+
+namespace MvcUtils
+{
+  [AttributeUsage(AttributeTargets.Property, AllowMultiple = false, Inherited = true)]
+  public class ShowAttribute : Attribute
+  {
+    private bool _showForEdit;
+    private bool _showForDisplay;
+
+    public ShowAttribute(bool forEdit, bool forDisplay)
+    {
+      _showForEdit = forEdit;
+      _showForDisplay = forDisplay;
+    }
+
+    public void Apply(ModelMetadata metadata)
+    {
+      metadata.ShowForDisplay = _showForDisplay;
+      metadata.ShowForEdit = _showForEdit;
+    }
+  }
+}
