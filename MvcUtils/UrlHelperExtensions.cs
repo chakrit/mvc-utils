344762ac2b20e997fd7437f9b807e29cb44f3a6a
Chakrit Wichian <chakrit@2nitedesign.com>
2011-03-17 16:35:48 +0700

    Removed helper already existed in MvcContrib and added an AbsoluteAction helper.

--- MvcUtils/UrlHelperExtensions.cs
+++ MvcUtils/UrlHelperExtensions.cs
@@ -1,18 +1,23 @@
 ﻿
-using System;
-using System.Linq.Expressions;
 using System.Web.Mvc;
 
 namespace MvcUtils
 {
   public static class UrlHelperExtensions
   {
-    public static string Action<TController>(this UrlHelper helper,
-      Expression<Action<TController>> action)
-      where TController : ControllerBase
+    public static string AbsoluteAction(this UrlHelper helper, string action,
+      string controller, object routeValues = null)
     {
-      var dict = action.ExtractRouteValues();
-      return helper.Action((string)dict["Action"], dict);
+      var requestUri = helper.RequestContext.HttpContext.Request.Url;
+
+      string actionUrl = (routeValues == null) ?
+        helper.Action(action, controller) :
+        helper.Action(action, controller, routeValues);
+
+      return string.Format("{0}://{1}{2}",
+        requestUri.Scheme,
+        requestUri.Authority,
+        actionUrl);
     }
   }
 }
